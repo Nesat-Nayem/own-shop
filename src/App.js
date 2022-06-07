18c3c1078d3b3b9ec4fb2adecf3c585252caf7c7
Nesat Nayem <nesatnayem042@gmail.com>
2022-06-08 05:51:39 +0600

    search field

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Footer></Footer>
+      <Banner></Banner>
+      {/* // <Footer></Footer> */}
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner-container overflow-hidden">
+      <div>
+        <img src="https://truelysell.com/assets/img/banner.jpg" />
+        <div className="banner w-100">
+          <div className="banner-form">
+            {/* search field form  */}
+            <form
+              action=""
+              className="d-md-flex justify-content-center banner-form-input"
+            >
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="what you need"
+                className="search-field search-icon"
+                style={{ color: "orange" }}
+              />
+
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Location (city or country)"
+                className="search-field location-icon"
+                style={{ color: "orange" }}
+              />
+              <button className="search-btn footer-search-btn p-3">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;

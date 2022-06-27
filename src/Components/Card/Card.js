e39bef1b173f8ed3ce950468277e643d80e3fb55
Nesat Nayem <nesatnayem042@gmail.com>
2022-06-27 07:58:02 +0600

    category filter

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,68 +1,60 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Rating } from 'react-simple-star-rating';
-import './Card.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeWishProduct } from '../../redux/slice';
-import { BoltRounded } from '@mui/icons-material';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Rating } from "react-simple-star-rating";
+import "./Card.css";
 
-const Card = ({product, addToCart, addToWishlist}) => {
-
+const Card = ({ product }) => {
   const { id, name, img, category, price, review } = product;
-  const dispatch = useDispatch();
-  const wishlist = useSelector(state => state.products.wishList)
-  const handleRemoveWishProduct = (product) => {
-    dispatch(removeWishProduct(product));
-}
 
-  const wished = wishlist.find(p => product.id === p.id)
+  return (
+    <div
+      style={{ marginTop: "80px" }}
+      className="col-lg-3 col-md-4 col-sm-6 col-xs-12 "
+    >
+      <div className="card h-100">
+        <img src={img} className="card-img-top" alt={name} />
 
-    return (
-        <div style={{marginTop:'80px'}}  className="col-lg-3 col-md-4 col-sm-6 col-xs-12 ">
-          <div  className="card h-100">
-            <img src={img} className="card-img-top" alt={name}/>
-            {/* {
-              wished ? 
-              <i className="fas fa-heart icon-cart" style={{color: 'crimson'}} onClick={() => handleRemoveWishProduct(product)}></i> 
-              : 
-              <i className="far fa-heart icon-cart" onClick={() => addToWishlist(product)}></i>
-            } */}
-
-            {/* <div className="add-to-cart-section d-flex flex-column">
-            <i className="fal fa-cart-plus icon-add-to-cart" onClick={() => addToCart(product)}></i>
-            <i className="far fa-eye icon-add-to-cart"></i>
-            </div> */}
-            <NavLink to={`/products/${category}/${id}`} className="text-decoration-none">
-            <div className="card-body d-flex flex-column align-items-start">
-                <Rating ratingValue={review * 20} size={18} readonly={true}/>
-              <p style={{
-                   color: '#3a9046',
-                  fontSize: '13px',
-                  margin: '5px 0'
-                  }}>
-                      {String(category).toLocaleUpperCase()}
-                </p>
-              <h2  className="card-title" style={{
-                  color: '#FF0080',
-                  fontSize: '18px',
-                  fontWeight:900,
-                  margin: '5px 0', 
-                  letterSpacing: '1px'
-                }}>
-                    {name}
-              </h2>
-              <p style={{
-                  color: '#3a9046',
-                  fontSize: '25px',
-                  margin: '0'
-               }}>
-                   ${price}
-              </p>
-            </div>
-          </NavLink>
+        <NavLink
+          to={`/products/${category}/${id}`}
+          className="text-decoration-none"
+        >
+          <div className="card-body d-flex flex-column align-items-start">
+            <Rating ratingValue={review * 20} size={18} readonly={true} />
+            <p
+              style={{
+                color: "#3a9046",
+                fontSize: "13px",
+                margin: "5px 0",
+              }}
+            >
+              {String(category).toLocaleUpperCase()}
+            </p>
+            <h2
+              className="card-title"
+              style={{
+                color: "#FF0080",
+                fontSize: "18px",
+                fontWeight: 900,
+                margin: "5px 0",
+                letterSpacing: "1px",
+              }}
+            >
+              {name}
+            </h2>
+            <p
+              style={{
+                color: "#3a9046",
+                fontSize: "25px",
+                margin: "0",
+              }}
+            >
+              ${price}
+            </p>
           </div>
-        </div>
-    );
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,68 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addProduct, addWishList, setProducts } from '../../redux/slice';
-import Card from '../Card/Card';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import './Products.css'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../../redux/slice";
+import Card from "../Card/Card";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import "./Products.css";
 
 const Products = () => {
+  const products = useSelector((state) => state.products.allProducts);
+  console.log("form product folder", products);
+  const cart = useSelector((state) => state.products.cart);
+  const dispatch = useDispatch();
 
-    const products = useSelector((state) => state.products.allProducts);
-    console.log('form product folder',products)
-    const cart = useSelector((state) => state.products.cart);
-    const dispatch = useDispatch();
+  useEffect(() => {
+    // fetch('http://localhost:5000/products')
+    fetch("http://localhost:7070/api/products/getProduct")
+      .then((res) => res.json())
+      .then((data) => dispatch(setProducts(data)));
+  }, []);
 
-    useEffect(() => {
-        // fetch('http://localhost:5000/products')
-        fetch('http://localhost:7070/api/products/getProduct')
-        .then(res => res.json())
-        .then(data => dispatch(setProducts(data)))
-    }, [])
+  const filterJobs = (e) => {
+    console.log(e.target.value);
+  };
 
-    // add to cart
-    const addToCart = (product) => {
-        const obj = { ...product };
-        obj.quantity = 1;
-        dispatch(addProduct(obj));
-    }
-    // add to wish list
-    const addToWishlist = (product) =>{
-        const obj = { ...product };
-        obj.quantity = 1;
-        dispatch(addWishList(obj));
-    }
+  return (
+    <div>
+      <Header></Header>
+      <img
+        className=" product-banner img-fluid"
+        src="https://i.postimg.cc/9X7ZHsYd/services-banner.webp"
+      />
+      <div className="container mx-auto procontrol d-flex justify-content-between  align-items-center">
+        <h6 style={{ color: "#FF0080" }} className="m-0 mt-5">
+          Total services found {products.length}
+        </h6>
 
-    // end of the code
-    useEffect(()=>{
-        // fetch('http://localhost:5000/products')
-        fetch('http://localhost:7070/api/products/getProduct')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    }, [])
-    
-
-
-    return (
-        <div>
-            <Header></Header>
-            <img  className=' product-banner img-fluid' src='https://i.postimg.cc/9X7ZHsYd/services-banner.webp' />
-        <div className="container mx-auto procontrol d-flex justify-content-between  align-items-center">
-            <h3 style={{color:'#FF0080'}} className='m-0 mt-5'>Total services found {products.length}</h3>
-
-        </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4 container mx-auto mb-5">
-          {
-            products?.map(pro=>{
-                  return(
-                    <Card key={pro.id} product={pro} addToCart={addToCart} addToWishlist={addToWishlist}></Card>
-                  )
-              })
-          }
-        </div>
-        <Footer></Footer>
+        <select
+          style={{
+            padding: "10px 30px",
+            backgroundColor: "#85F4FF",
+            border: "none",
+            outline: "none",
+          }}
+          name=""
+          id=""
+          // value={category}
+          className=" px-2 py-2 "
+          onChange={filterJobs}
+        >
+          <option value="all">All Services</option>
+          <option value="Intriory"> Intriory</option>
+          <option value="Computer">Computer</option>
+          <option value="Construction">Construction</option>
+          <option value="Carwash">Carwash</option>
+          <option value="Cleaning">Cleaning</option>
+          <option value="Electrical">Electrical</option>
+        </select>
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4 container mx-auto mb-5">
+        {products?.map((pro) => {
+          return (
+            <Card key={pro.id} product={pro}></Card>
+            // {/* <Card products={products} ></Card> */}
+          );
+        })}
+      </div>
+      <Footer></Footer>
     </div>
-    );
+  );
 };
 
 export default Products;
